e139e2124f7b42e5315e580b77041e5d0324eb58
Restyled.io <commits@restyled.io>
2023-11-09 08:45:35 +0000

    Restyled by autopep8

--- app.py
+++ app.py
@@ -14,16 +14,17 @@ with st.sidebar:
         st.image(logo, use_column_width="always")
 
     # Ask user to input their API Token
-    genta_api_key = st.text_input("Genta API key", key="chatbot_api_key", type="password")
-    
+    genta_api_key = st.text_input(
+        "Genta API key", key="chatbot_api_key", type="password")
+
     # Ask user to select their model of choice
     model_selected = st.selectbox("Choose your LLM you would like to try:",
-                              ("Merak", "Starstreak", "DukunLM"))
-    
+                                  ("Merak", "Starstreak", "DukunLM"))
+
     # Add Clear Chat button
     if st.button('Clear chat'):
         st.session_state.messages = st.session_state.messages[0:1]
-    
+
     # Add advanced option
     advanced = st.toggle('Advanced mode')
     if advanced:
@@ -31,7 +32,7 @@ with st.sidebar:
         temperature = st.slider(
             ':blue[Temperature]',
             0.0, 2.0, 1.0)
-        
+
         # Set the model max token to be generated
         max_length = st.slider(
             ":blue[Maximum lenght]",
@@ -56,7 +57,8 @@ st.caption("A simple demonstration of GentaAPI for chat purposes")
 
 # Initialize a new message if there isnt a message in session state
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "Halo, saya adalah asisten anda, ada yang bisa saya bantu?"}]
+    st.session_state["messages"] = [
+        {"role": "assistant", "content": "Halo, saya adalah asisten anda, ada yang bisa saya bantu?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
@@ -79,13 +81,11 @@ if prompt := st.chat_input():
         response = API.ChatCompletion(chat_history=st.session_state.messages[1:], model_name=model_selected, max_new_tokens=max_length,
                                       temperature=temperature, top_p=top_p, repetition_penalty=rep_penalty)
     else:
-        response = API.ChatCompletion(chat_history=st.session_state.messages[1:], model_name=model_selected, max_new_tokens=1024)
+        response = API.ChatCompletion(
+            chat_history=st.session_state.messages[1:], model_name=model_selected, max_new_tokens=1024)
     response = response[0][0]["generated_text"]
 
     # Display the response
-    st.session_state.messages.append({"role":"assistant", "content":response})
+    st.session_state.messages.append(
+        {"role": "assistant", "content": response})
     st.chat_message("assistant").write(response)
-
-
-
-    
